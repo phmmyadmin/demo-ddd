38585574d726447e35445108cf67ba427373c780
iaw49299980 <gayarre.pablo@gmail.com>
2021-02-20 18:46:32 +0100

    add behat

--- apps/mooc/backend/src/Controller/HealthCheck/HealthCheckGetController.php
+++ apps/mooc/backend/src/Controller/HealthCheck/HealthCheckGetController.php
@@ -1,17 +1,30 @@
 <?php
 
-declare(strict_types=1);
+declare(strict_types = 1);
 
 namespace CodelyTv\Apps\Mooc\Backend\Controller\HealthCheck;
 
+use CodelyTv\Shared\Domain\RandomNumberGenerator;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class HealthCheckGetController
+final class HealthCheckGetController
 {
-    public function __invoke(): JsonResponse
+    private $generator;
+
+    public function __construct(RandomNumberGenerator $generator)
     {
-        return new JsonResponse([
-            'status' => 'ok'
-        ]);
+        $this->generator = $generator;
+    }
+
+    public function __invoke(Request $request): Response
+    {
+        return new JsonResponse(
+            [
+                'mooc-backend' => 'ok',
+                'rand'         => $this->generator->generate(),
+            ]
+        );
     }
 }
